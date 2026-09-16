c9fa593ef9903da4a60fd4f0ab303f6066cabb61
JihadPD9 <muhammadjihad1099@gmail.com>
2026-09-16 16:28:59 +0700

    Menghapus Array pada BuktiPiketContoller

--- backend/app/Http/Controllers/Api/BuktiPiketController.php
+++ backend/app/Http/Controllers/Api/BuktiPiketController.php
@@ -54,7 +54,7 @@ class BuktiPiketController extends Controller
             'foto_1'    => 'required|image|mimes:jpeg,png,jpg|max:2048',
             'foto_2'    => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
             'deskripsi' => 'nullable|string',
-            'tasks'     => 'required|array', // Array ID task yang dikerjakan
+            'tasks'     => 'required', // Array ID task yang dikerjakan
             'tasks.*'   => 'exists:tasks,id',
         ]);
 
